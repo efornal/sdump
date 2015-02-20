e207ae49a5ef7152590808cd59b055ce6c6931ae
esteban <efornal@gmail.com>
2015-02-20 13:53:19 -0300

    migraciones para bases y servidores

---
+++ app/migrations/0002_base.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Base',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True)),
+                ('nombre', models.CharField(max_length=64)),
+                ('usuario', models.CharField(max_length=64)),
+                ('contrasenia', models.CharField(max_length=64)),
+                ('descripcion', models.TextField(null=True)),
+            ],
+            options={
+                'db_table': 'bases',
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- app/models.py
+++ app/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Servidor(models.Model):
+    id = models.AutoField(primary_key=True,null=False)
+    nombre = models.CharField(max_length=64,null=False)
+    ip = models.CharField(max_length=64,null=True)
+    puerto = models.IntegerField(null=True)
+    motor = models.CharField(max_length=64,null=True)
+    descripcion = models.TextField(null=True)
+    
+    class Meta:
+        db_table = 'servidores'   
+
+    def __str__(self):
+        return self.nombre
+
+
+class Base(models.Model):
+    id = models.AutoField(primary_key=True,null=False)
+    nombre = models.CharField(max_length=64,null=False)
+    usuario = models.CharField(max_length=64,null=False)
+    contrasenia = models.CharField(max_length=64,null=False)
+    descripcion = models.TextField(null=True)
+    
+    class Meta:
+        db_table = 'bases'   
+
+    def __str__(self):
+        return self.nombre
+
+
+
+
+
+        
