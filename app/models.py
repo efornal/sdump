6c70b5c166f3907755b7902501036425b50de46f
esteban <efornal@gmail.com>
2015-02-23 09:13:25 -0300

    se agregan modelos y sus relaciones

--- app/models.py
+++ app/models.py
@@ -3,12 +3,24 @@ from django.db import models
 # Create your models here.
 
 
+class Grupo(models.Model):
+    id = models.AutoField(primary_key=True,null=False)
+    nombre = models.CharField(max_length=100,null=False)
+    directorio = models.TextField(null=True)
+    
+    class Meta:
+        db_table = 'grupos'   
+
+    def __str__(self):
+        return self.nombre
+
+    
 class Servidor(models.Model):
     id = models.AutoField(primary_key=True,null=False)
-    nombre = models.CharField(max_length=64,null=False)
-    ip = models.CharField(max_length=64,null=True)
+    nombre = models.CharField(max_length=100,null=False)
+    ip = models.CharField(max_length=100,null=True)
     puerto = models.IntegerField(null=True)
-    motor = models.CharField(max_length=64,null=True)
+    motor = models.CharField(max_length=100,null=True)
     descripcion = models.TextField(null=True)
     
     class Meta:
@@ -19,11 +31,13 @@ class Servidor(models.Model):
 
 
 class Base(models.Model):
-    id = models.AutoField(primary_key=True,null=False)
-    nombre = models.CharField(max_length=64,null=False)
-    usuario = models.CharField(max_length=64,null=False)
-    contrasenia = models.CharField(max_length=64,null=False)
+    id = models.AutoField(primary_key=True, null=False)
+    nombre = models.CharField(max_length=100, null=False)
+    usuario = models.CharField(max_length=100, null=False)
+    contrasenia = models.CharField(max_length=100, null=False)
     descripcion = models.TextField(null=True)
+    servidor = models.ForeignKey(Servidor, null=True)
+    grupo = models.ForeignKey(Grupo, null=True)
     
     class Meta:
         db_table = 'bases'   
@@ -32,7 +46,15 @@ class Base(models.Model):
         return self.nombre
 
 
+class Usuario(models.Model):
+    id = models.AutoField(primary_key=True,null=False)
+    nombre  = models.CharField(max_length=100,null=False)
+    usuario = models.CharField(max_length=100,null=False)
+    grupos = models.ManyToManyField(Grupo)
+     
+    class Meta:
+        db_table = 'usuarios'   
 
+    def __str__(self):
+        return self.nombre
 
-
-        
