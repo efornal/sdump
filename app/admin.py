88516d2852b65e2bd1861dbe9216875106496556
esteban <efornal@gmail.com>
2015-02-23 10:39:26 -0300

    registro de modelos en módulo de administración

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from app.models import Grupo, Servidor, Base, Usuario
 
 # Register your models here.
+admin.site.register(Grupo)
+admin.site.register(Servidor)
+admin.site.register(Base)
+admin.site.register(Usuario)
+
+
+
+
+
