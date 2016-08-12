41b8e0f6df8c69d46335a94ed3dd453801a3af79
esteban <efornal@gmail.com>
2016-08-12 09:14:25 -0300

    added logo

--- sdump/settings.tpl.py
+++ sdump/settings.tpl.py
@@ -115,6 +115,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
 LOGIN_URL='/login/'
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
