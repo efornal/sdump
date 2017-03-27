5bd9a2c672620dd57a6b856e005a087350406cdf
esteban <efornal@gmail.com>
2017-03-27 12:34:19 -0300

    added dump sharing restrictions

--- app/views.py
+++ app/views.py
@@ -66,8 +66,6 @@ def to_date_according_to_text(s_date):
     return None
 
 
-
-
 def describe_file (file_path):
 
     descrived_file = {}
@@ -86,7 +84,7 @@ def describe_file (file_path):
                                           .filter(name=file_path) \
                                           .first()
         if not share_file_related is None:
-            link_to_share_file = reverse('api_export_dump' ,
+            link_to_share_file = reverse('api_share_dump' ,
                                          kwargs={'filename':share_file_related.hash})
             
         descrived_file = {'file_path': file_path,
@@ -848,10 +846,25 @@ def api_get_database_id(request):
         return HttpResponse('500 Internal Server Error', status=500)
 
 
+def api_share_dump(request, filename=None):
+    if 'filename' is None:
+        logging.error("Undefined file name")
+        return HttpResponse('400 Invalid request', status=400)
+    
+    try:
+        share_file = Share.objects.get(hash=filename)
+    except Exception as e:
+        logging.error ("ERROR Exception: {}".format(e))
+        return HttpResponse('500 Internal Server Error', status=500)
 
-def api_export_dump(request, filename=None):
-    logging.warning(filename)
-    share_file = Share.objects.get(hash=filename)
+    if not have_file_permissions(request.user.username,share_file.name):
+        logging.error("User without permissions to share the dump")
+        return HttpResponse('401 Unauthorized', status=401)
+
+    if not share_file.database.alow_sharing:
+        logging.error("The dump is not allowed to be shared.")
+        return HttpResponse('401 Unauthorized', status=401)
+    
     logging.warning("Exporting file {} with hash {}".format(share_file.name,share_file.hash))
     response = FileResponse(FileWrapper(file(share_file.name, 'rb')),
                             content_type='application/gzip')
