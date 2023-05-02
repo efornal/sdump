e32098856154b2eaa5f0b2e554b6044283693d07
Esteban <efornal@gmail.com>
2023-05-02 11:43:46 -0300

    improves output information in entrypoint.sh

--- container-build/dumps/entrypoint.sh
+++ container-build/dumps/entrypoint.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ -n ${DUMPS_USER_NAME} && -n ${DUMPS_USER_PASSWORD} ]]; then
-    echo -e ${DUMPS_USER_NAME}:${DUMPS_USER_PASSWORD} | chpasswd
-    echo "Updated password of user ${DUMPS_USER_NAME}."
+if [[ -n ${DUMPS_USER_NAME} && -n ${DUMPS_USER_PASS} ]]; then
+    echo -e ${DUMPS_USER_NAME}:${DUMPS_USER_PASS} | chpasswd
+    if [ $? -eq 0 ]; then
+	echo -e "The password of user ${DUMPS_USER_NAME} has been updated."
+    else
+	echo -e "Error updating password of user ${DUMPS_USER_NAME}."
+    fi
+else
+    echo -e "User ${DUMPS_USER_NAME} password update omitted, undefined data"
 fi
 
 service ssh restart 
