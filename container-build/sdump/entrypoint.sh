7fa8ac0f6be84ba3cb2e32d0b71e16ed6a28e2db
Esteban <efornal@gmail.com>
2023-10-02 09:11:27 -0300

    added directory dumps.conf.d and defines app permissions for directory

--- container-build/sdump/entrypoint.sh
+++ container-build/sdump/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+mkdir -p /srv/dumps.conf.d
 mkdir -p /srv/dumps
+chown -R www-data.www-data /srv/dumps.conf.d
 chown -R www-data.www-data /srv/dumps
 
 python manage.py compilemessages
