371d9c1af65f0c787d10b149b30f846c5152ac0f
Esteban <efornal@gmail.com>
2023-10-02 08:56:01 -0300

    defines app permissions for directory

--- container-build/sdump/entrypoint.sh
+++ container-build/sdump/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+mkdir -p /srv/dumps
+chown -R www-data.www-data /srv/dumps
+
 python manage.py compilemessages
 python manage.py collectstatic --noinput
 python manage.py migrate --database db_owner
