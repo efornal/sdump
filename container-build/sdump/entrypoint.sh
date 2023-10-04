43bc6434ff5815fd2800b2b6b22201a5b650e51a
Esteban <efornal@gmail.com>
2023-10-04 14:09:20 -0300

    improved dockerfile build process

--- container-build/sdump/entrypoint.sh
+++ container-build/sdump/entrypoint.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-mkdir -p /srv/dumps.conf.d
-chown -R www-data.www-data /srv/dumps.conf.d
-
-mkdir -p /srv/dumps
-chown -R dumpserver.www-data /srv/dumps
-chmod -R 765 /srv/dumps
-
 python manage.py compilemessages
 python manage.py collectstatic --noinput
 python manage.py migrate --database db_owner
